2d0fda52ed3417f9bcc27048168dbbf7c387078f
Claive Monteza <claivemonteza@outlook.com>
2021-10-19 22:27:22 +0200

    Change de name

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/country-rest'));
+app.use(express.static('./dist/Country-Client-REST'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/Country-Client-REST/'}),
