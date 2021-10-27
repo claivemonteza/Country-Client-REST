ce0619cc134746d61a0277acaa17794335e4a926
Claive Monteza <claivemonteza@outlook.com>
2021-10-27 14:53:51 +0200

    Update V3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/country-client-rest'));
+app.use(express.static(__dirname + '/dist/Country-Client-REST'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/country-client-rest/index.html'));});
+'/dist/Country-Client-REST/index.html'));});
 app.listen(process.env.PORT || 8080);
